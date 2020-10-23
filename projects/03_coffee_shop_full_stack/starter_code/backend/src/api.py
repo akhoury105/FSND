9f4026eb41aa62bed6505cdd8132b752cc9e8b79
Aaron Khoury <akhoury105@gmail.com>
2020-10-23 14:08:15 -0400

    Adds route for getting detailed list of drinks for baristas

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -48,7 +48,19 @@ def get_drinks():
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
-
+@app.route('/drinks', methods=['GET'])
+@requires_auth(permission='get:drink-detail')
+def get_drinks():
+    selection = Drink.query.all()
+    if len(selection) == 0:
+        abort(422)
+    drinks = [selection.long() for drink in selection]
+    if len(drinks) == 0:
+        abort(404)
+    return jsonify({
+        'success': True,
+        'drinks': drinks
+    })
 
 '''
 @TODO implement endpoint
